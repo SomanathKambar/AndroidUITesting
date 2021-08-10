f111cb9772c19d15620948cd106a5cd0d0653ec8
somu2 <KambarSomanath@gmail.com>
2021-08-10 13:32:18 +0530

    Combining matchers
    . in this we are verifying the title of
      the activity which has no id and part
      of toolbar but without fetching title from
      tetView.
    . Here we are using BoundedMatcher with getTitle
      method from Toolbar.

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.kt
@@ -1,18 +1,23 @@
 package com.example.androiduitesting
 
 
+import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.Toolbar
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,12 +39,40 @@ class MainActivityTest {
 
     @Test
     fun toolbarTitle() {
-        //to get the view without id
+        //to get the view0 without id
         Espresso.onView(
             allOf(
                 isAssignableFrom(AppCompatTextView::class.java),
                 ViewMatchers.withParent(isAssignableFrom(Toolbar::class.java))
             )
         ).check(ViewAssertions.matches(withText(R.string.title)))
+    }
+
+    @Test
+    fun toolBarTitleWithCustomMatcher(){
+        val title = InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.title)
+        Espresso.onView(isAssignableFrom(Toolbar::class.java))
+            .check(ViewAssertions.matches(withToolbarTitle(title)))
+    }
+
+    private fun withToolbarTitle(expectedTitle: String): Matcher<View>{
+        return object : BoundedMatcher<View, Toolbar>(Toolbar::class.java){
+            /**
+             * Generates a description of the object.  The description may be part of a
+             * a description of a larger object of which this is just a component, so it
+             * should be worded appropriately.
+             *
+             * @param description
+             * The description to be built or appended to.
+             */
+            override fun describeTo(description: Description?) {
+                description?.appendText("with toolbar text : $expectedTitle")
+            }
+
+            override fun matchesSafely(toolbar: Toolbar?): Boolean {
+                return expectedTitle == toolbar?.title
+            }
+
+        }
     }
 }
